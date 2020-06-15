338d89a1c4a1e8af9d06b0138a722f434d7c4c70
nick black <dankamongmen@gmail.com>
2020-06-14 20:47:19 -0400

    upgrade to notcurses 1.5.1

--- src/main.rs
+++ src/main.rs
@@ -29,19 +29,14 @@ fn main() {
     unsafe{
         let _ = libc::setlocale(libc::LC_ALL, std::ffi::CString::new("").unwrap().as_ptr());
         let opts: ffi::notcurses_options = ffi::notcurses_options {
-            inhibit_alternate_screen: true,
             loglevel: 0,
             termtype: std::ptr::null(),
-            retain_cursor: false,
-            suppress_banner: false,
-            no_winch_sighandler: false,
-            no_quit_sighandlers: false,
             renderfp: std::ptr::null_mut(),
             margin_t: 8,
             margin_r: 8,
             margin_b: 8,
             margin_l: 8,
-            flags: ffi::NCOPTION_INHIBIT_SETLOCALE,
+            flags: (ffi::NCOPTION_INHIBIT_SETLOCALE | ffi::NCOPTION_NO_ALTERNATE_SCREEN) as u64,
         };
         let nc = ffi::notcurses_init(&opts, libc_stdout());
         let stdplane = ffi::notcurses_stdplane(nc);
